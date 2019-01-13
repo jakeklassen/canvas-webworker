344127cf7d054beb9c1dfd46a433b60a082ac591
Jake Klassen <jklassendev@gmail.com>
2019-01-13 18:35:02 -0500

    some stress tests and deploy steps

--- src/loop.ts
+++ src/loop.ts
@@ -1,7 +1,38 @@
 import { createFixedLengthList } from './utils/fixed-length-list';
 
-const square = { position: { x: 0, y: 100 }, velocity: { x: 100 } };
+const rand = (min: number, max: number) => Math.random() * (max - min) + min;
 
+const createSquare = () => {
+  const position = { x: rand(0, 630), y: rand(0, 470) };
+  const velocity = { x: rand(0, 300), y: rand(0, 300) };
+
+  return {
+    position,
+    velocity,
+    color: `rgba(${rand(0, 255)}, ${rand(0, 255)}, ${rand(0, 255)}, 1)`,
+    collider: {
+      width: 10,
+      height: 10,
+      get left() {
+        return position.x;
+      },
+      get right() {
+        return position.x + this.width;
+      },
+      get top() {
+        return position.y;
+      },
+      get bottom() {
+        return position.y + this.height;
+      },
+    },
+  };
+};
+
+const squares = Array.from({ length: 4000 }, () => createSquare());
+
+let width;
+let height;
 let wbuffer: OffscreenCanvas;
 let wctx: OffscreenCanvasRenderingContext2D;
 let last = performance.now();
@@ -14,21 +45,50 @@ function frame(hrt: DOMHighResTimeStamp) {
   // Seems like the worker `hrt` is not reset on reload so
   // override for now.
   hrt = performance.now();
-  let dt = (hrt - last) / 1000;
+  dt = (hrt - last) / 1000;
   dts.add(dt);
   const averageFps = Math.round(
     1 / (dts.list.reduce((a, b) => a + b, 0) / dts.list.length),
   );
 
   wctx.clearRect(0, 0, 640, 480);
-  wctx.fillStyle = 'red';
-  square.position.x += square.velocity.x * dt;
-  wctx.fillRect(square.position.x, square.position.y, 10, 10);
 
+  for (const square of squares) {
+    wctx.fillStyle = square.color;
+    square.position.x += square.velocity.x * dt;
+    square.position.y += square.velocity.y * dt;
+
+    if (square.collider.right > width) {
+      square.position.x = width - square.collider.width;
+      square.velocity.x *= -1;
+    } else if (square.collider.left < 0) {
+      square.position.x = 0;
+      square.velocity.x *= -1;
+    }
+
+    if (square.collider.bottom > height) {
+      square.position.y = height - square.collider.height;
+      square.velocity.y *= -1;
+    } else if (square.collider.top < 0) {
+      square.position.y = 0;
+      square.velocity.y *= -1;
+    }
+
+    wctx.fillRect(
+      square.position.x,
+      square.position.y,
+      square.collider.width,
+      square.collider.height,
+    );
+  }
+
+  wctx.fillStyle = 'black';
+  wctx.fillRect(540, 0, 100, 60);
   wctx.fillStyle = 'white';
-  wctx.fillText(averageFps.toString(), 10, 20);
-  wctx.fillText(`{ x: ${square.position.x}, y: ${square.position.y} }`, 10, 40);
-  wctx.fillText(`{ hrt: ${hrt} }`, 10, 60);
+  wctx.fillText(averageFps.toString(), width - 30, 20);
+  wctx.fillText(`squares: ${squares.length}`, width - 80, 40);
+  // wctx.fillText(`{ x: ${square.position.x}, y: ${square.position.y} }`, 10, 40);
+  // wctx.fillText(`{ hrt: ${hrt} }`, 10, 60);
 
   last = hrt;
 }
@@ -37,6 +97,9 @@ self.onmessage = event => {
   if (event.data.event === 'start') {
     wbuffer = event.data.buffer as OffscreenCanvas;
     wctx = wbuffer.getContext('2d');
+
+    width = wbuffer.width;
+    height = wbuffer.height;
 
     requestAnimationFrame(frame);
   }
